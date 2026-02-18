4b885baab1fbc7ef4d1226f41055de7bab269cae
Clifford Bressette <cliffordbressette@mcindi.com>
2026-02-18 09:58:09 -0500

    Add more robust admin handling of cash models

--- cash/admin.py
+++ cash/admin.py
@@ -2,8 +2,26 @@ from django.contrib import admin
 
 from .models import Category, Fund, Expense, Receipt
 
-admin.site.register(Category)
-admin.site.register(Fund)
-admin.site.register(Expense)
-admin.site.register(Receipt)
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description', 'family')
+    list_filter = ('family',)
+    search_fields = ('name', 'description')
 
+@admin.register(Fund)
+class FundAdmin(admin.ModelAdmin):
+    list_display = ('user', 'family', 'amount', 'date', 'note')
+    list_filter = ('family', 'date')
+    search_fields = ('note',)
+
+@admin.register(Expense)
+class ExpenseAdmin(admin.ModelAdmin):
+    list_display = ('user', 'family', 'category', 'amount', 'date', 'note')
+    list_filter = ('family', 'category', 'date')
+    search_fields = ('note',)
+
+@admin.register(Receipt)
+class ReceiptAdmin(admin.ModelAdmin):
+    list_display = ('expense', 'family', 'uploaded_at')
+    list_filter = ('family', 'uploaded_at')
+    search_fields = ('expense__note',)
